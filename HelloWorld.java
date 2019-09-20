79743a958ec0588fdf392818a74713f59f8b51ea
danielpgp1012 <daniel.pachecogutierrez@epfl.ch>
2019-09-20 12:19:46 +0200

    Ultimate Question

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello SwEng!");	
+    System.out.println("Hello SwEng!");
+    System.out.println("The ultimate question of Life, The Universe, and Everything is: " + computeUltimateQuestion());	
   }
 }
 public static String computeUltimateQuestion() {
