8ea942d133ff31119877af2876359ce69d9ad5d8
danielpgp1012 <daniel.pachecogutierrez@epfl.ch>
2019-09-20 12:14:21 +0200

    Compute Ultimate Question

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello SwEng!");
+    System.out.println("Hello SwEng!");	
   }
+}
+public static String computeUltimateQuestion() {
+	return "still searching...";
 }
